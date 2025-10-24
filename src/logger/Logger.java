cfd8c61540dbace216a275d8cdd15c69022a31b8
KarimovFamil <famil.ker2001@gmail.com>
2025-10-24 12:11:48 +0400

    Required and Logger classes is add

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -1,4 +1,19 @@
 package logger;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class Logger {
+
+    public static void log(String mesage) throws IOException {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ayxan.txt", true))) {
+
+            writer.write(LocalDateTime.now() + " " + mesage + "\n");
+
+        }
+
+    }
 }
