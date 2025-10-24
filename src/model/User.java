0258506be5de3851b6761112c59bb0b62459b37e
zarifa <zerifexasmemmedli@gmail.com>
2025-10-24 11:58:18 +0400

    user class and Enum is created

--- src/model/User.java
+++ src/model/User.java
@@ -3,12 +3,59 @@ package model;
 public class User {
 
     private String name;
+    private String surname;
+    private int age;
+    private String username;
+    private String password;
+    private Role role;
+
 
     public String getName() {
         return name;
     }
 
+    public String getSurName() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole(){
+        return role;
+    }
+
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setSurName(String surname){
+        this.surname = surname;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setUserName(String username){
+        this.username=username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setRole(Role role){
+        this.role = role;
     }
 }
