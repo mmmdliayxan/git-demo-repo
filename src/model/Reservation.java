8b8e0687f727553c232cb18bde132d0560892b3b
AbulfazGurbanli <qurbanliebulfez@gmail.com>
2025-10-24 12:02:11 +0400

    reservasion is created

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,4 +1,49 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Reservation {
+    private User user;
+    private ReservationSeatNumber seatNumber;
+    private LocalDateTime dateTime;
+    private Movie movie;
+
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setReservationSeatNumber(ReservationSeatNumber seatNumber){
+        this.seatNumber = seatNumber;
+
+    }
+
+    public ReservationSeatNumber getSeatNumber() {
+        return seatNumber;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setSeatNumber(ReservationSeatNumber seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 }
--- src/model/ReservationSeatNumber.java
+++ src/model/ReservationSeatNumber.java
@@ -1,4 +1,25 @@
 package model;
 
 public class ReservationSeatNumber {
-}
+
+        private int setNumber;
+
+        public int getSetNumber() {
+            return setNumber;
+        }
+
+        public void setSetNumber(int setNumber) {
+            this.setNumber = setNumber;
+        }
+
+        public ReservationSeatNumber (int setNumber){
+            this.setNumber = setNumber;
+
+        }
+
+        @Override
+        public String toString(){
+            return "seat number :" + setNumber;
+        }
+    }
+
